b7af4c25862ca040831f43232ccfcab930ff2d7b
Manjunatha P <manjunathashivas0p@gmail.com>
2026-07-15 12:37:44 +0530

    Time: 39 ms (82.6%), Space: 14 MB (49.09%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -4,11 +4,13 @@ class Solution(object):
         answer = 0
         left = 0
         for right in range(len(nums)):
+            #multiples the numbers in nums
             product *= nums[right]
-
+            #if the value surpases the k value
             while product >=k:
                 product =product // nums[left]
                 left +=1
+            # when it fits then add it to the answr as count
             answer += right - left +1
         return answer
         
