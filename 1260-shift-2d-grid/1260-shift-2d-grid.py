41af5acf62e1c0d65edfafe035103ae5d709cf92
Manjunatha P <manjunathashivas0p@gmail.com>
2026-07-20 20:48:40 +0530

    Time: 275 ms (8.42%), Space: 12.5 MB (100%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        m,n  = len(grid), len(grid[0])
+
+        for _ in range(k):
+            last = grid[m-1][n-1]
+            for i in range(m-1,-1,-1):
+                for j in range(n-1,-1,-1):
+                    if i ==0 and j==0:
+                        grid[0][0]=last
+                    elif j==0:
+                        grid[i][j]= grid[i-1][n-1]
+                    else:
+                        grid[i][j]= grid[i][j-1]
+        return grid
