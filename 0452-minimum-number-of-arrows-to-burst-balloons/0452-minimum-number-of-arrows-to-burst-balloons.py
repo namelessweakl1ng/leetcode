38ab1ce030a2970139d0acd92cdc64ec650f2788
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-03 17:18:05 +0530

    Time: 223 ms (15.08%), Space: 43.9 MB (59.98%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def findMinArrowShots(self, points):
-        if not points:
+        if not points: # nothin exist then return 0
             return 0
-        points.sort()
-        start, end = points[0]
-        arrow=1
-        for s,e in points[1:]:
-            if end>= s:
-                start = max(s,start)
+        points.sort() # sorts the array by the index[0]
+        start, end = points[0] # initialize the start and the end so that the points can be used to compare
+        arrow=1 # if there is a value in points then obviously we will need one arrow
+        for s,e in points[1:]: # take the loop from index 1 till the end
+            if end>= s: # this says it overlaps
+                start = max(s,start) # finding the intersection of the two intervals to throw a arraow at
                 end = min(end ,e)
             else:
-                arrow+=1
-                start, end = s,e
+                arrow+=1 # if the range is not a intersection then you will need another arraow
+                start, end = s,e # reinitialize the start and the end so that you can keep the loop going 
         return arrow
