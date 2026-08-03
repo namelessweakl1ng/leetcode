c7c2f34588c9a45b89e028d5127e5b6a86b9b528
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-03 08:25:15 +0530

    Time: 140 ms (25.38%), Space: 66.4 MB (67.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,8 +5,23 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        arr=[]
-        while head:
-            arr.append(head.val)
-            head= head.next
-        return arr == arr[::-1]
+        slow = fast = head
+        # go fast and slow to find the middle in the slow
+        while fast and fast.next: # loop till the fast is at the last node or the null
+            slow = slow.next
+            fast = fast.next.next
+        # rotate the 2nd half of the list
+        prev = None
+        while slow: # till the slow becomes null
+            temp = slow.next
+            slow.next= prev
+            prev= slow
+            slow = temp
+        # after rotating compare it and return
+        left,right = head, prev # prev will be at the end and the slow will be null
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right  = right.next
+        return True
