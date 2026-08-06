7e05cc7be84ac25dc5c188af427bbe47c439e5ed
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-06 16:31:46 +0530

    Time: 199 ms (8.32%), Space: 26.8 MB (15.57%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -5,15 +5,15 @@ class Solution(object):
         curr_sum = 0
         l = 0
         for r in range(len(nums)):
-            curr_sum += nums[r]
-            count[nums[r]]+=1
+            curr_sum += nums[r] # add this to the curr_sum integer
+            count[nums[r]]+=1 # when you see a number increase the count inside the dict in the default dict
 
-            if r-l+1>k:
-                count[nums[l]]-=1
-                curr_sum-=nums[l]
-                if count[nums[l]]==0:
+            if r-l+1>k: # if the window is too high then increase the left
+                count[nums[l]]-=1 # you are not seeing the left item anymore
+                curr_sum-=nums[l] # subtract the thing youre deleting 
+                if count[nums[l]]==0: # if it goes to 0 then delete from the window so that it dont mess with the len(count)
                     count.pop(nums[l])
-                l+=1
+                l+=1 # go forward
 
             if r- l+1 == k == len(count):
                 res= max(res,curr_sum)
