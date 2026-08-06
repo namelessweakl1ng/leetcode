ef8164d57c03bfc535906995adc548dbdf5b0cc8
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-06 16:43:51 +0530

    Time: 140 ms (67.52%), Space: 25.7 MB (76.38%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def maximumSubarraySum(self, nums, k):
-        res = 0
-        count = defaultdict(int)
         curr_sum = 0
-        l = 0
+        prev_idx = {}
+        l=0
+        res =0
+
         for r in range(len(nums)):
-            curr_sum += nums[r] # add this to the curr_sum integer
-            count[nums[r]]+=1 # when you see a number increase the count inside the dict in the default dict
-
-            if r-l+1>k: # if the window is too high then increase the left
-                count[nums[l]]-=1 # you are not seeing the left item anymore
-                curr_sum-=nums[l] # subtract the thing youre deleting 
-                if count[nums[l]]==0: # if it goes to 0 then delete from the window so that it dont mess with the len(count)
-                    count.pop(nums[l])
-                l+=1 # go forward
-
-            if r- l+1 == k == len(count):
-                res= max(res,curr_sum)
+            curr_sum +=nums[r]
+            i = prev_idx.get(nums[r],-1)
+            prev_idx[nums[r]] =r
+            while l<=i or r-l+1>k:
+                curr_sum-=nums[l]
+                l+=1
+            if r-l+1 ==k:
+                res = max(res,curr_sum)
         return res
