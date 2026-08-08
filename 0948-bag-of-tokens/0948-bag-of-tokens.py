020815222edc82e43239e6e47d2eae55ca01b592
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-08 23:21:57 +0530

    Time: 4 ms (53.99%), Space: 12.5 MB (18.4%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -5,15 +5,15 @@ class Solution(object):
         
         l,r = 0 , len(tokens)-1
         while l<=r:
-            if power >=tokens[l]:
+            if power >=tokens[l]: # face up cuz you can play all the small toens in the List
                 power -= tokens[l]
                 l+=1
                 score+=1
                 res = max(res,score)
-            elif score > 0:
+            elif score > 0: # if you have a score but not have power then from the right face down
                 power +=tokens[r]
                 r-=1
                 score -=1
-            else:
+            else: # you stuck with 0 and 0 power and score so break and return 
                 break
         return res
