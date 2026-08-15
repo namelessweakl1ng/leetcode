953a7db05cc71dda9f2252fec0db440773fea12c
Manjunatha P <manjunathashivas0p@gmail.com>
2026-08-15 13:35:25 +0530

    Time: 44 ms (80.95%), Space: 21.7 MB (95.24%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.py
@@ -10,5 +10,5 @@ class Solution(object):
         
         for num in nums:
             if num!=0:
-                return len(nums)-1
+                return len(nums)-1 # logic is that if the reult is 0 then if you remove just one non zero element the total result become non- zero
         return 0
